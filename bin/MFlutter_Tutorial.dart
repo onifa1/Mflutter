673ff74ea59592e26736c91f8f30e8c989887d8c
onifa1 <onifadeifeoluwa1@gmail.com>
2021-01-15 20:18:23 +0100

    first commit

---
+++ bin/MFlutter_Tutorial.dart
@@ -0,0 +1,5 @@
+import 'package:MFlutter_Tutorial/MFlutter_Tutorial.dart' as MFlutter_Tutorial;
+
+void main(List<String> arguments) {
+  print('Hello world: ${MFlutter_Tutorial.calculate()}!');
+}
---
+++ test/MFlutter_Tutorial_test.dart
@@ -0,0 +1,8 @@
+import 'package:MFlutter_Tutorial/MFlutter_Tutorial.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
